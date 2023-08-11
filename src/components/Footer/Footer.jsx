484631c6b6966697e08d7aaf655bac333696cb6c
ibragim-ast <ibragim.ast@gmail.com>
2023-08-12 00:32:28 +0300

    add Footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,38 @@
+import "./Footer.css";
+
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer__container">
+        <p className="footer__text">
+          Учебный проект Яндекс.Практикум х BeatFilm.
+        </p>
+        <div className="footer__bottom">
+          <span className="footer__year">© 2023</span>
+          <ul className="footer__links">
+            <li>
+              <a
+                className="footer__link"
+                href="https://practicum.yandex.ru/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Яндекс.Практикум
+              </a>
+            </li>
+            <li>
+              <a
+                className="footer__link"
+                href="https://github.com/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Github
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  );
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,6 +5,7 @@ import Promo from "./Promo/Promo";
 import AboutProject from "./AboutProject/AboutProject";
 import Techs from "./Techs/Techs";
 import AboutMe from "./AboutMe/AboutMe";
+import Footer from "../Footer/Footer";
 
 const Main = () => {
   return (
@@ -14,6 +15,7 @@ const Main = () => {
       <AboutProject />
       <Techs />
       <AboutMe />
+      <Footer />
     </>
   );
 };
