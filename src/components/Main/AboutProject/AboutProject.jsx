c00a64b2f15aeeaa85221091eb06a5ee11ffa634
ibragim-ast <ibragim.ast@gmail.com>
2023-08-11 15:21:09 +0300

    add aboutProject component

---
+++ src/components/Main/AboutProject/AboutProject.jsx
@@ -0,0 +1,39 @@
+import "./AboutProject.css";
+
+export default function AboutProject() {
+  return (
+    <section className="about">
+      <div className="about__container">
+        <h2 className="about__title">О проекте</h2>
+        <div className="about__cards-container">
+          <div className="about__card">
+            <h3 className="about__card-title">
+              Дипломный проект включал 5 этапов
+            </h3>
+            <p className="about__card-text">
+              Составление плана, работу над бэкендом, вёрстку, добавление
+              функциональности и финальные доработки.
+            </p>
+          </div>
+          <div className="about__card">
+            <h3 className="about__card-title">
+              На выполнение диплома ушло 5 недель
+            </h3>
+            <p className="about__card-text">
+              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+              соблюдать, чтобы успешно защититься.
+            </p>
+          </div>
+        </div>
+        <div className="about__time-scale">
+          <p className="about__one-week">1 неделя</p>
+          <p className="about__four-week">4 недели</p>
+        </div>
+        <div className="about__tech">
+          <p className="about__back">Back-end</p>
+          <p className="about__front">Front-end</p>
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,12 +2,14 @@ import React from "react";
 
 import Header from "../Header/Header";
 import Promo from "./Promo/Promo";
+import AboutProject from "./AboutProject/AboutProject";
 
 const Main = () => {
   return (
     <>
       <Header />
       <Promo />
+      <AboutProject />
     </>
   );
 };
