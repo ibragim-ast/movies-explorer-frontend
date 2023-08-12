909dbda1dd2cd42efacbd199306cad0d3373b0f8
ibragim-ast <ibragim.ast@gmail.com>
2023-08-12 21:12:41 +0300

    fix navigate error

--- src/components/ErrorPage/ErrorPage.jsx
+++ src/components/ErrorPage/ErrorPage.jsx
@@ -1,17 +1,15 @@
-import { Navigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./ErorPage.css";
 
 export default function ErrorPage() {
-  const handleGoBack = () => {
-    return <Navigate to=".." />;
-  };
+  const navigate = useNavigate();
 
   return (
     <section className="error-page">
       <div className="error-page__message">
         <h1 className="error-page__title">404</h1>
         <p className="error-page__text">Страница не найдена</p>
-        <button className="error-page__btn" onClick={handleGoBack}>
+        <button className="error-page__btn" onClick={() => navigate(-1)}>
           Назад
         </button>
       </div>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -8,12 +8,12 @@ export default function Header() {
       <div className="header__container">
         <img className="header__logo" src={logo} alt="Логотип" />
         <div className="header__links">
-          <Link to="/sign-up">
+          <Link to="/signup">
             <button className="header__link header__register-link">
               Регистрация
             </button>
           </Link>
-          <Link to="/sign-in">
+          <Link to="/signin">
             <button className="header__link header__login-link">Войти</button>
           </Link>
         </div>
