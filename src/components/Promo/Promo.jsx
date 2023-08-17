fdf0ebc00cec1466e2e73b5e971001e3d6126798
ibragim-ast <ibragim.ast@gmail.com>
2023-08-17 16:56:14 +0300

    add background image in promo

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -1,15 +1,11 @@
-import pattern from "../../assets/images/pattern.svg";
 import "./Promo.css";
 
 const Promo = () => {
   return (
     <section className="promo">
-      <div className="promo__container">
-        <h1 className="promo__title">
-          Учебный проект студента факультета Веб-разработки.
-        </h1>
-        <img src={pattern} alt="узор" className="promo__pattern" />
-      </div>
+      <h1 className="promo__title">
+        Учебный проект студента факультета Веб-разработки.
+      </h1>
     </section>
   );
 };
