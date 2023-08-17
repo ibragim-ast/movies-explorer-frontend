de98ef5f487bc6e3188fdbef302a0bfaf36de41d
ibragim-ast <ibragim.ast@gmail.com>
2023-08-17 19:18:15 +0300

    fix errors in landing

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -4,36 +4,34 @@ import Title from "../Title/Title";
 export default function AboutProject() {
   return (
     <section className="about">
-      <div className="about__container">
-        <Title title="О проекте"></Title>
-        <div className="about__cards-container">
-          <div className="about__card">
-            <h3 className="about__card-title">
-              Дипломный проект включал 5 этапов
-            </h3>
-            <p className="about__card-text">
-              Составление плана, работу над бэкендом, вёрстку, добавление
-              функциональности и финальные доработки.
-            </p>
-          </div>
-          <div className="about__card">
-            <h3 className="about__card-title">
-              На выполнение диплома ушло 5 недель
-            </h3>
-            <p className="about__card-text">
-              У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
-              соблюдать, чтобы успешно защититься.
-            </p>
-          </div>
+      <Title title="О проекте"></Title>
+      <div className="about__cards-container">
+        <div className="about__card">
+          <h3 className="about__card-title">
+            Дипломный проект включал 5 этапов
+          </h3>
+          <p className="about__card-text">
+            Составление плана, работу над бэкендом, вёрстку, добавление
+            функциональности и финальные доработки.
+          </p>
         </div>
-        <div className="about__time-scale">
-          <p className="about__one-week">1 неделя</p>
-          <p className="about__four-week">4 недели</p>
+        <div className="about__card">
+          <h3 className="about__card-title">
+            На выполнение диплома ушло 5 недель
+          </h3>
+          <p className="about__card-text">
+            У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
+            соблюдать, чтобы успешно защититься.
+          </p>
         </div>
-        <div className="about__tech">
-          <p className="about__back">Back-end</p>
-          <p className="about__front">Front-end</p>
-        </div>
+      </div>
+      <div className="about__time-scale">
+        <p className="about__one-week">1 неделя</p>
+        <p className="about__four-week">4 недели</p>
+      </div>
+      <div className="about__tech">
+        <p className="about__back">Back-end</p>
+        <p className="about__front">Front-end</p>
       </div>
     </section>
   );
