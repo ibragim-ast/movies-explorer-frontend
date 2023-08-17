56542754a1fe1722ef0895db4eebaec50bb85db6
ibragim-ast <ibragim.ast@gmail.com>
2023-08-17 19:47:59 +0300

    refactoring styles in aboutProjects

--- src/components/AboutProject/AboutProject.jsx
+++ src/components/AboutProject/AboutProject.jsx
@@ -3,35 +3,43 @@ import Title from "../Title/Title";
 
 export default function AboutProject() {
   return (
-    <section className="about">
+    <section className="about-project">
       <Title title="О проекте"></Title>
-      <div className="about__cards-container">
-        <div className="about__card">
-          <h3 className="about__card-title">
+      <div className="about-project__cards-container">
+        <div className="about-project__card">
+          <h3 className="about-project__card-title">
             Дипломный проект включал 5 этапов
           </h3>
-          <p className="about__card-text">
+          <p className="about-project__card-text">
             Составление плана, работу над бэкендом, вёрстку, добавление
             функциональности и финальные доработки.
           </p>
         </div>
-        <div className="about__card">
-          <h3 className="about__card-title">
+        <div className="about-project__card">
+          <h3 className="about-project__card-title">
             На выполнение диплома ушло 5 недель
           </h3>
-          <p className="about__card-text">
+          <p className="about-project__card-text">
             У каждого этапа был мягкий и жёсткий дедлайн, которые нужно было
             соблюдать, чтобы успешно защититься.
           </p>
         </div>
       </div>
-      <div className="about__time-scale">
-        <p className="about__one-week">1 неделя</p>
-        <p className="about__four-week">4 недели</p>
+      <div className="about-project__roadmap-scale">
+        <p className="about-project__info about-project__info_type_one-week">
+          1 неделя
+        </p>
+        <p className="about-project__info about-project__info_type_four-week">
+          4 недели
+        </p>
       </div>
-      <div className="about__tech">
-        <p className="about__back">Back-end</p>
-        <p className="about__front">Front-end</p>
+      <div className="about-project__roadmap-tech">
+        <p className="about-project__fullstack about-project__fullstack_type_back">
+          Back-end
+        </p>
+        <p className="about-project__fullstack about-project__fullstack_type_front">
+          Front-end
+        </p>
       </div>
     </section>
   );
