c5ad924c794921b04d5dea68aaea878fcfd92ea1
ibragim-ast <ibragim.ast@gmail.com>
2023-08-19 14:50:02 +0300

    add adaptive 768 register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -6,39 +6,37 @@ import "./Register.css";
 export default function Register() {
   return (
     <section className="register">
-      <div className="register__container">
-        <img className="register__logo" src={logo} alt="Логотип" />
-        <form className="register__form">
-          <h1 className="register__title">Добро пожаловать!</h1>
-          <div className="register__form-container">
-            <div className="register__input-container">
-              <span className="register__input-title">Имя</span>
-              <input className="register__input" type="name" name="name" />
-              <span className="register__input-error"></span>
-            </div>
-            <div className="register__input-container">
-              <span className="register__input-title">E-mail</span>
-              <input className="register__input" type="email" name="email" />
-              <span className="register__input-error"></span>
-            </div>
-            <div className="register__input-container">
-              <span className="register__input-title">Пароль</span>
-              <input
-                className="register__input"
-                type="password"
-                name="password"
-              />
-              <span className="register__input-error"></span>
-            </div>
+      <img className="register__logo" src={logo} alt="Логотип" />
+      <form className="register__form">
+        <h1 className="register__title">Добро пожаловать!</h1>
+        <div className="register__form-container">
+          <div className="register__input-container">
+            <span className="register__input-title">Имя</span>
+            <input className="register__input" type="name" name="name" />
+            <span className="register__input-error"></span>
           </div>
-          <Button className="register__submit-btn" text="Зарегистрироваться" />
-        </form>
-        <div className="register__message">
-          <p>Уже зарегистрированы?</p>
-          <Link className="login-link" to="/signin">
-            Войти
-          </Link>
+          <div className="register__input-container">
+            <span className="register__input-title">E-mail</span>
+            <input className="register__input" type="email" name="email" />
+            <span className="register__input-error"></span>
+          </div>
+          <div className="register__input-container">
+            <span className="register__input-title">Пароль</span>
+            <input
+              className="register__input"
+              type="password"
+              name="password"
+            />
+            <span className="register__input-error"></span>
+          </div>
         </div>
+        <Button className="register__submit-btn" text="Зарегистрироваться" />
+      </form>
+      <div className="register__message">
+        <p>Уже зарегистрированы?</p>
+        <Link className="login-link" to="/signin">
+          Войти
+        </Link>
       </div>
     </section>
   );
