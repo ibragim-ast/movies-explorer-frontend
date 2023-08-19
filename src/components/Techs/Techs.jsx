4ab1f1b5670dfb548a2033cb1c7b95f1152d3c5e
ibragim-ast <ibragim.ast@gmail.com>
2023-08-19 23:56:30 +0300

    add adaptive 320 Techs

--- src/components/Techs/Techs.jsx
+++ src/components/Techs/Techs.jsx
@@ -6,19 +6,17 @@ import "./Techs.css";
 export default function Techs() {
   return (
     <section className="techs">
-      <div className="techs__container">
-        <Title title="Технологии" />
-        <h3 className="techs__title">7 технологий</h3>
-        <p className="techs__text">
-          На курсе веб-разработки мы освоили технологии, которые применили в
-          дипломном проекте.
-        </p>
-        <ul className="techs__list">
-          {STACK.map((tech) => (
-            <TechIcon key={tech} title={tech} />
-          ))}
-        </ul>
-      </div>
+      <Title title="Технологии" />
+      <h3 className="techs__title">7 технологий</h3>
+      <p className="techs__text">
+        На курсе веб-разработки мы освоили технологии, которые применили в
+        дипломном проекте.
+      </p>
+      <ul className="techs__list">
+        {STACK.map((tech) => (
+          <TechIcon key={tech} title={tech} />
+        ))}
+      </ul>
     </section>
   );
 }
