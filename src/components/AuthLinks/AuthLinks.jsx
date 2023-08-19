4cad19511838ded9c854c7780ef99b940870d00b
ibragim-ast <ibragim.ast@gmail.com>
2023-08-19 22:37:43 +0300

    add adaptive 320 promo

--- src/components/AuthLinks/AuthLinks.jsx
+++ src/components/AuthLinks/AuthLinks.jsx
@@ -5,10 +5,10 @@ export default function AuthLinks() {
   return (
     <div className="auth-links">
       <Link to="/signup">
-        <button className="auth__link auth__register-link">Регистрация</button>
+        <button className="auth__link">Регистрация</button>
       </Link>
       <Link to="/signin">
-        <button className="auth__link auth__login-link">Войти</button>
+        <button className="auth__link auth__link_type_login">Войти</button>
       </Link>
     </div>
   );
