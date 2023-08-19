4e55da9c84c5a15af8112514556e374b1997daeb
ibragim-ast <ibragim.ast@gmail.com>
2023-08-19 23:16:06 +0300

    add adaptive 320 aboutProject

--- src/components/AuthLinks/AuthLinks.jsx
+++ src/components/AuthLinks/AuthLinks.jsx
@@ -5,7 +5,9 @@ export default function AuthLinks() {
   return (
     <div className="auth-links">
       <Link to="/signup">
-        <button className="auth__link">Регистрация</button>
+        <button className="auth__link auth__link_type_register">
+          Регистрация
+        </button>
       </Link>
       <Link to="/signin">
         <button className="auth__link auth__link_type_login">Войти</button>
