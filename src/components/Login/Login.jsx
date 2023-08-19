88703d1163a6355a5acd375e886c0fa29e5d4d75
ibragim-ast <ibragim.ast@gmail.com>
2023-08-19 14:54:11 +0300

    add adaptive 768 login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -3,33 +3,31 @@ import { Link } from "react-router-dom";
 import Button from "../Button/Button";
 import "./Login.css";
 
-export default function Login({ onLogin }) {
+export default function Login() {
   return (
     <section className="login">
-      <div className="login__container">
-        <img className="login__logo" src={logo} alt="Логотип" />
-        <form className="login__form">
-          <h1 className="login__title">Рады видеть!</h1>
-          <div className="login__form-container">
-            <div className="login__input-container">
-              <span className="login__input-title">E-mail</span>
-              <input className="login__input" type="email" name="email" />
-              <span className="login__input-error"></span>
-            </div>
-            <div className="login__input-container">
-              <span className="login__input-title">Пароль</span>
-              <input className="login__input" type="password" name="password" />
-              <span className="login__input-error"></span>
-            </div>
+      <img className="login__logo" src={logo} alt="Логотип" />
+      <form className="login__form">
+        <h1 className="login__title">Рады видеть!</h1>
+        <div className="login__form-container">
+          <div className="login__input-container">
+            <span className="login__input-title">E-mail</span>
+            <input className="login__input" type="email" name="email" />
+            <span className="login__input-error"></span>
           </div>
-          <Button className="register__submit-btn" text="Войти" />
-        </form>
-        <div className="login__message">
-          <p>Ещё не зарегистрированы?</p>
-          <Link className="login-link" to="/signup">
-            Регистрация
-          </Link>
+          <div className="login__input-container">
+            <span className="login__input-title">Пароль</span>
+            <input className="login__input" type="password" name="password" />
+            <span className="login__input-error"></span>
+          </div>
         </div>
+        <Button className="register__submit-btn" text="Войти" />
+      </form>
+      <div className="login__message">
+        <p>Ещё не зарегистрированы?</p>
+        <Link className="login-link" to="/signup">
+          Регистрация
+        </Link>
       </div>
     </section>
   );
