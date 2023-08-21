db8b9c71edf7d789b33a5eb8ca6e16f59945c9d3
ibragim-ast <ibragim.ast@gmail.com>
2023-08-21 18:28:49 +0300

    add adaptive Login

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -5,30 +5,36 @@ import "./Login.css";
 
 export default function Login() {
   return (
-    <section className="login">
-      <img className="login__logo" src={logo} alt="Логотип" />
-      <form className="login__form">
-        <h1 className="login__title">Рады видеть!</h1>
-        <div className="login__form-container">
-          <div className="login__input-container">
-            <span className="login__input-title">E-mail</span>
-            <input className="login__input" type="email" name="email" />
-            <span className="login__input-error"></span>
+    <>
+      <section className="login">
+        <img className="login__logo" src={logo} alt="Логотип" />
+        <form className="login__form">
+          <h1 className="login__title">Рады видеть!</h1>
+          <div className="login__form-container">
+            <div className="login__input-container">
+              <label className="login__input-title">E-mail</label>
+              <input className="login__input" type="email" name="email" />
+              <span className="login__input-error"></span>
+            </div>
+            <div className="login__input-container">
+              <label className="login__input-title">Пароль</label>
+              <input className="login__input" type="password" name="password" />
+              <span className="login__input-error"></span>
+            </div>
           </div>
-          <div className="login__input-container">
-            <span className="login__input-title">Пароль</span>
-            <input className="login__input" type="password" name="password" />
-            <span className="login__input-error"></span>
-          </div>
-        </div>
-        <Button className="register__submit-btn" text="Войти" />
-      </form>
-      <div className="login__message">
+          <Button
+            className="register__submit-btn"
+            modifier="register"
+            text="Войти"
+          />
+        </form>
+      </section>
+      <div className="login__cta">
         <p>Ещё не зарегистрированы?</p>
         <Link className="login-link" to="/signup">
           Регистрация
         </Link>
       </div>
-    </section>
+    </>
   );
 }
