ebdd13d46f4c425327843e1c6ddc188453f5b633
ibragim-ast <ibragim.ast@gmail.com>
2023-08-24 14:46:17 +0300

    добавляет ссылки в portfolio

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -8,26 +8,33 @@ export default function Portfolio() {
         <li className="portfolio__project">
           <a
             className="portfolio__link link-hover"
-            href="example.com"
+            href="https://ibragim-ast.github.io/burgers/"
             target="_blank"
+            rel="noreferrer"
           >
-            Статичный сайт
+            Сайт закусочной
           </a>
           <span className="project__arrow">↗</span>
         </li>
         <li className="portfolio__project">
           <a
             className="portfolio__link link-hover"
-            href="example.com"
+            href="https://ibragim-ast.github.io/russian-travel/"
             target="_blank"
+            rel="noreferrer"
           >
-            Адаптивный сайт
+            Сайт о путешествиях по России
           </a>
           <span className="project__arrow">↗</span>
         </li>
         <li className="portfolio__project">
-          <a className="portfolio__link link-hover" href="example.com">
-            Одностраничное приложение
+          <a
+            className="portfolio__link link-hover"
+            href="https://github.com/ibragim-ast/react-mesto-api-full-gha"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Приложение "Место"
           </a>
           <span className="project__arrow">↗</span>
         </li>
