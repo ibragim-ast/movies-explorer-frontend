7a3bb4bc0409ccfa90adf4016c6bff3c5f7bc609
ibragim-ast <ibragim.ast@gmail.com>
2023-08-24 18:32:12 +0300

    добавляет section в pagenotfound

--- src/components/PageNotFound/PageNotFound.jsx
+++ src/components/PageNotFound/PageNotFound.jsx
@@ -5,7 +5,7 @@ const PageNotFound = () => {
   const navigate = useNavigate();
 
   return (
-    <main className="not-found">
+    <section className="not-found">
       <div className="not-found__info">
         <h3 className="not-found__title">404</h3>
         <p className="not-found__subtitle">Страница не найдена</p>
@@ -16,7 +16,7 @@ const PageNotFound = () => {
       >
         Назад
       </button>
-    </main>
+    </section>
   );
 };
 
