a8d0bcba9d0edef76f2fdf563195522573759a29
ibragim-ast <ibragim.ast@gmail.com>
2023-08-24 18:36:09 +0300

    добавляет section в searchform

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -5,16 +5,22 @@ import "./SearchForm.css";
 
 const SearchForm = () => {
   return (
-    <form className="search-form" name="search-movie">
-      <div className="search-form__string">
-        <img src={searchIcon} alt="поиск" className={"search-form__icon"} />
-        <input className="search-form__input" type="text" placeholder="Фильм" />
-        <Button modifier="search" text="Найти" />
-        <span className="search-form__line"></span>
-        <FitterCheckbox />
-      </div>
-      <span className="search-form__error"></span>
-    </form>
+    <section>
+      <form className="search-form" name="search-movie">
+        <div className="search-form__string">
+          <img src={searchIcon} alt="поиск" className={"search-form__icon"} />
+          <input
+            className="search-form__input"
+            type="text"
+            placeholder="Фильм"
+          />
+          <Button modifier="search" text="Найти" />
+          <span className="search-form__line"></span>
+          <FitterCheckbox />
+        </div>
+        <span className="search-form__error"></span>
+      </form>
+    </section>
   );
 };
 
